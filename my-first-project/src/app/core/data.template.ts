afbec70ecdb14ec0eb45a0667f392b5b56506444
Rob-Willson <ribrob@gmail.com>
2021-02-26 18:02:03 +0000

    API request result now parsed via a data intermediate to allow splitting the data points up into individual objects. This seems 'OK' but there's probably a cleaner way to do it.

--- my-first-project/src/app/core/data.template.ts
+++ my-first-project/src/app/core/data.template.ts
@@ -1,34 +1,63 @@
-// Data output format from crypto API endpoint
-export class RequestData {
-    public constructor(
-      public Response: string,
-      public Message: string,
-      public HasWarning: boolean,
-      public Type: number,
-      public Data: Data
-    )
-    {}
-  }
-  export class Data {
-    constructor(
-      public Aggregated: boolean,
-      public TimeFrom: number,
-      public TimeTo: number,
-      public Data: Datum[]
-    )
-    {}
-  }
-  export class Datum {
-    constructor(
-      public time: number,
-      public high: number,
-      public low: number,
-      public open: number,
-      public volumefrom: number,
-      public volumeto: number,
-      public close: number,
-      public conversionType: string,
-      public conversionSymbol: string
-    )
-    {}
-  }
+// Raw data output format from crypto API endpoint
+// Don't touch variable names unless you want to break stuff
+export class CryptoDataRaw {
+  constructor(
+    public Response: string,
+    public Message: string,
+    public HasWarning: boolean,
+    public Type: number,
+    public Data: CryptoDataRawSummary
+  )
+  {}
+}
+export class CryptoDataRawSummary {
+  constructor(
+    public Aggregated: boolean,
+    public TimeFrom: number,
+    public TimeTo: number,
+    public Data: CryptoDataRawSummaryData[]
+  )
+  {}
+}
+export class CryptoDataRawSummaryData {
+  constructor(
+    public time: number,
+    public high: number,
+    public low: number,
+    public open: number,
+    public volumefrom: number,
+    public volumeto: number,
+    public close: number,
+    public conversionType: string,
+    public conversionSymbol: string
+  )
+  {}
+}
+
+// Processed data object ready for use
+// TODO: Observables?
+export class CryptoData {
+  private constructor(
+    public readonly currency: string,
+    public readonly time: number,
+    public readonly high: number,
+    public readonly low: number
+  )
+  {}
+
+  public static ParseFromJSON (rawJsonData: any): CryptoData[] {
+    let dataRaw: CryptoDataRaw = rawJsonData
+    let cryptoDataProcessed: CryptoData[] = [];
+
+    for (let i = 0; i < dataRaw.Data.Data.length; i++) {
+      cryptoDataProcessed.push(new CryptoData(
+        "currencyId" + i,
+        dataRaw.Data.Data[i].time,
+        dataRaw.Data.Data[i].high,
+        dataRaw.Data.Data[i].low
+      ));
+    }
+
+    return cryptoDataProcessed;
+  }  
+}
--- my-first-project/src/app/core/http.service.ts
+++ my-first-project/src/app/core/http.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RequestData } from './data.template';
+import { CryptoData } from './data.template';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
   // TODO: Why is this "undefined" assignment required?
-  requestData: RequestData | undefined;
+  dataProcessed: CryptoData | undefined;
 
   constructor(private httpClient : HttpClient) { }
 
@@ -16,10 +16,13 @@ export class HttpService {
     
     this.httpClient.get<any>(url).subscribe(
       response => {
-        console.log(response);
-        this.requestData = response;
+        let allDataProcessed: CryptoData[] = CryptoData.ParseFromJSON(response);
+
+        allDataProcessed.forEach(element => {
+          console.log(element);
+        });
       }
     );
   }
-
+  
 }
