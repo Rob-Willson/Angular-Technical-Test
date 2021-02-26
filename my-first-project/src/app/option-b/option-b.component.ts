09891f7fb85f388085a2db73a90a25a5da59302d
Rob-Willson <ribrob@gmail.com>
2021-02-26 12:59:05 +0000

    Quick and dirty API call to cryptocompare.com, just logging response to console

--- my-first-project/src/app/option-b/option-b.component.ts
+++ my-first-project/src/app/option-b/option-b.component.ts
@@ -1,15 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+// Parsed data output from crypto API endpoint
+export class RequestData {
+  constructor(
+    public Response: string,
+    public Message: string,
+    public HasWarning: boolean,
+    public Type: number,
+    public Data: Data
+  )
+  {}
+}
+export class Data {
+  constructor(
+    public Aggregated: boolean,
+    public TimeFrom: number,
+    public TimeTo: number,
+    public Data: Datum[]
+  )
+  {}
+}
+export class Datum {
+  constructor(
+    public time: number,
+    public high: number,
+    public low: number,
+    public open: number,
+    public volumefrom: number,
+    public volumeto: number,
+    public close: number,
+    public conversionType: string,
+    public conversionSymbol: string
+  )
+  {}
+}
 
 @Component({
   selector: 'app-option-b',
   templateUrl: './option-b.component.html',
   styleUrls: ['./option-b.component.css']
 })
+
 export class OptionBComponent implements OnInit {
   
-  constructor() { }
+  // TODO: Why is this "undefined" assignment required?
+  requestData: RequestData | undefined;
+
+  constructor(private httpClient : HttpClient) { }
 
   ngOnInit(): void {
+    console.log("init");
+    this.getRequest();
+  }
+
+  getRequest() {
+    let url = 'https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=10';
+    this.httpClient.get<any>(url).subscribe(
+      response => {
+        console.log(response);
+        this.requestData = response;
+      }
+    );
   }
 
 }
