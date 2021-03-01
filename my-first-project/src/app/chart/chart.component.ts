5e7afb075ad4f3dc9b18887a6ad81bc1c9430bae
Rob-Willson <ribrob@gmail.com>
2021-03-01 11:07:37 +0000

    Converted chart from box plot to time/date line graph

--- my-first-project/src/app/chart/chart.component.ts
+++ my-first-project/src/app/chart/chart.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../core/http.service';
-import { CryptoData } from '../core/data.template';
+import { CryptoData, CryptoDataRaw } from '../core/data.template';
 import { Coin } from '../core/coin.template';
 import * as d3 from 'd3';
-import * as d3Scale from 'd3';
-import * as d3Shape from 'd3';
-import * as d3Array from 'd3';
-import * as d3Axis from 'd3';
 
 @Component({
   selector: 'app-chart',
@@ -22,10 +18,10 @@ export class ChartComponent implements OnInit {
   // TODO: API key?
 
   private svg;
-  private margin = 50;
-  private width = 600 - (this.margin * 2);
-  private height = 400 - (this.margin * 2);
-  
+  private margin: number = 50;
+  private width: number = 800 - (this.margin * 2);
+  private height: number = 400 - (this.margin * 2);
+
   // All supported crytocurrencies with their default display values
   public coins: Coin[] = [
     new Coin("BTC", true),
@@ -43,18 +39,25 @@ export class ChartComponent implements OnInit {
   constructor(private httpService : HttpService) { }
 
   ngOnInit(): void {
+
     this.httpService.getRequest(this.buildUrl());
-    
+
     // TODO: This should be the data from the API call
-    let data = [
-      {"Year": "2016", "Assets": "405.9" },
-      {"Year": "2017", "Assets": "409.4" },
-      {"Year": "2018", "Assets": "643.3" },
-      {"Year": "2019", "Assets": "618.2" }
+    let data: CryptoData[] = [
+      new CryptoData("A", 1614614800, 48000.0, 43000.0),
+      new CryptoData("B", 1614624900, 53000.0, 41000.0),
+      new CryptoData("C", 1614635000, 51000.0, 46000.0),
+      new CryptoData("D", 1614645100, 54000.0, 51000.0),
+      new CryptoData("E", 1614655200, 59000.0, 52000.0),
+      new CryptoData("A", 1614665300, 48000.0, 43000.0),
+      new CryptoData("B", 1614675400, 53000.0, 41000.0),
+      new CryptoData("C", 1614685500, 51000.0, 46000.0),
+      new CryptoData("D", 1614695600, 54000.0, 51000.0),
+      new CryptoData("E", 1614705700, 59000.0, 52000.0)
     ];
-        
+
     this.createSvg();
-    this.drawBars(data);
+    this.drawPlot(data);
   }
 
   onSelectCoin(coinId: any) {
@@ -84,7 +87,8 @@ export class ChartComponent implements OnInit {
   }
 
   private createSvg(): void {
-    this.svg = d3.select("figure#bar")
+    this.svg = d3
+    .select("figure#bar")
     .append("svg")
     .attr("width", this.width + (this.margin * 2))
     .attr("height", this.height + (this.margin * 2))
@@ -92,40 +96,61 @@ export class ChartComponent implements OnInit {
     .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
   }
 
-  private drawBars(data: any[]): void {
+  private drawPlot(data: CryptoData[]): void {
     // Create the X-axis band scale
-    const x = d3.scaleBand()
-    .range([0, this.width])
-    .domain(data.map(d => d.Year))
-    .padding(0.2);
+    // TODO: This has a weird cast. It's required because d3.min/max don't support returning undefined
+    const x = d3
+    .scaleTime()
+    .domain([d3.min(data, d => d.time) as number, d3.max(data, d => d.time) as number])
+    .range([0, this.width]);
+
+    // Create the Y-axis band scale
+    // TODO: This has a weird cast. It's required because d3.min/max don't support returning undefined
+    const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, d => d.high) as number])
+    .range([this.height, 0]);
 
     // Draw the X-axis on the DOM
-    this.svg.append("g")
+    this.svg
+    .append("g")
     .attr("transform", "translate(0," + this.height + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
 
-    // Create the Y-axis band scale
-    const y = d3.scaleLinear()
-    .domain([0, 800])
-    .range([this.height, 0]);
-
     // Draw the Y-axis on the DOM
-    this.svg.append("g")
+    this.svg
+    .append("g")
     .call(d3.axisLeft(y));
 
-    // Create and fill the bars
-    this.svg.selectAll("bars")
+    // Draw the lines
+    const points: [number, number][] = data.map(
+      d => [x(new Date(d.time)), y(d.high)]
+    );
+    this.svg
+      .append('g')
+      .append('path')
+      .attr('id', 'line')
+      .style('fill', 'none')
+      .style('stroke', 'steelblue')
+      .style('stroke-width', '2px')
+      .attr("d", d3.line()
+        .x(d => d[0])
+        .y(d => d[1])(points));
+    
+
+    // Plot the dots
+    this.svg.append("g")
+    .selectAll("dot")
     .data(data)
     .enter()
-    .append("rect")
-    .attr("x", d => x(d.Year))
-    .attr("y", d => y(d.Assets))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => this.height - y(d.Assets))
-    .attr("fill", "#ff8080");
-  }  
+    .append("circle")
+    .attr("cx", d => x(d.time))
+    .attr("cy", d => y(d.high))
+    .classed("chart-point", true);
+
+  }
 
 }
