7d0ad67bc29089827eb15efd25cdcaa9daab0645
Rob-Willson <ribrob@gmail.com>
2021-03-01 14:14:13 +0000

    Added high/low line data to graph, removed dots, added curve to lines/areas

--- my-first-project/src/app/chart/chart.component.ts
+++ my-first-project/src/app/chart/chart.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../core/http.service';
-import { interval, Subscription } from 'rxjs';
-import { CryptoData } from '../core/data.template';
-import { Coin } from '../core/coin.template';
-import * as d3 from 'd3';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../core/http.service";
+import { interval, Subscription } from "rxjs";
+import { CryptoData } from "../core/data.template";
+import { Coin } from "../core/coin.template";
+import * as d3 from "d3";
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  selector: "app-chart",
+  templateUrl: "./chart.component.html",
+  styleUrls: ["./chart.component.css"]
 })
 
 export class ChartComponent implements OnInit {
@@ -22,8 +22,6 @@ export class ChartComponent implements OnInit {
   private margin: number = 50;
   private width: number = 800 - (this.margin * 2);
   private height: number = 400 - (this.margin * 2);
-
-  subscription: Subscription;
 
   // All supported crytocurrencies with their default display values
   public coins: Coin[] = [
@@ -39,11 +37,12 @@ export class ChartComponent implements OnInit {
     new Coin("XMR", false),
   ];
 
+  subscription: Subscription;
+
   constructor(private httpService : HttpService) {
     this.subscription = interval(5000).subscribe(x => {
       this.createSvg();
       this.updatePlot();
-      console.log("done");
     });
   }
 
@@ -62,28 +61,12 @@ export class ChartComponent implements OnInit {
     });
   }
 
-  onSelectCoin(coinId: any) {
-    console.log("onSelectCoin(" + coinId + ") CALLED");
-
-    this.coins.forEach(coin => {
-      if(coinId == coin.id) {
-        coin.active = !coin.active;
-      }
-    });
-
-    let logMessage: string = "";
-    this.coins.forEach(coin => {
-      logMessage += coin.id + ": " + coin.active + ", ";
-    });
-    console.log(logMessage);
-  }
-
   private buildUrl() : string {
     let url : string = this.urlBase + "?";
     // Request requires a querying currency (fsym) and a standard (tsym) to compare it with
     url += "fsym=" + this.urlCurrencyId + "&";
     url += "tsym=" + this.urlCurrencyStandardId;
-    // Cryptocompare API uses base 0 for it's counts, but most humans don't
+    // Cryptocompare API uses base 0 for it"s counts, but most humans don"t
     url += "&limit=" + (this.urlCount - 1);
     return url;
   }
@@ -103,17 +86,17 @@ export class ChartComponent implements OnInit {
 
   private drawPlot(data: CryptoData[]): void {
     // Create the X-axis band scale
-    // TODO: This has a weird cast. It's required because d3.min/max don't support returning undefined
+    // TODO: This has a weird cast. It"s required because d3.min/max don"t support returning undefined
     const x = d3
     .scaleTime()
     .domain([d3.min(data, d => d.time) as number, d3.max(data, d => d.time) as number])
     .range([0, this.width]);
 
     // Create the Y-axis band scale
-    // TODO: This has a weird cast. It's required because d3.min/max don't support returning undefined
+    // TODO: This has a weird cast. It"s required because d3.min/max don"t support returning undefined
     const y = d3
     .scaleLinear()
-    .domain([0, d3.max(data, d => d.high) as number])
+    .domain([0, d3.max(data, d => d.average()) as number])
     .range([this.height, 0]);
 
     // Draw the X-axis on the DOM
@@ -131,35 +114,93 @@ export class ChartComponent implements OnInit {
     .call(d3.axisLeft(y));
 
     this.plotData(x, y, data);
-
   }
 
   private plotData(x, y, data: CryptoData[]): void {
     // Draw the lines
     const points: [number, number][] = data.map(
-      d => [x(new Date(d.time)), y(d.high)]
+      d => [x(new Date(d.time)), y(d.average())]
     );
     this.svg
-      .append('g')
-      .append('path')
-      .attr('id', 'line')
-      .style('fill', 'none')
-      .style('stroke', 'steelblue')
-      .style('stroke-width', '2px')
+      .append("g")
+      .append("path")
+      .attr("id", "line")
+      .style("fill", "none")
+      .style("stroke", "rgba(70, 130, 180, 1)")
+      .style("stroke-width", "2.5px")
       .attr("d", d3.line()
+        .curve(d3.curveBasis)
         .x(d => d[0])
         .y(d => d[1])(points));
     
+    const pointsHighs: [number, number][] = data.map(
+      d => [x(new Date(d.time)), y(d.high)]
+    );
+    this.svg
+      .append("g")
+      .append("path")
+      .attr("id", "line")
+      .style("fill", "none")
+      .style("stroke", "rgba(70, 130, 180, 1)")
+      .style("stroke-width", "1.5px")
+      .attr("d", d3.line()
+        .curve(d3.curveBasis)
+        .x(d => d[0])
+        .y(d => d[1])(pointsHighs));
+
+    const pointsLows: [number, number][] = data.map(
+      d => [x(new Date(d.time)), y(d.low)]
+    );
+    this.svg
+      .append("g")
+      .append("path")
+      .attr("id", "line")
+      .style("fill", "none")
+      .style("stroke", "rgba(70, 130, 180, 1)")
+      .style("stroke-width", "1.5px")
+      .attr("d", d3.line()
+        .curve(d3.curveBasis)
+        .x(d => d[0])
+        .y(d => d[1])(pointsLows));     
+
+    const pointsArea: [number, number][] = pointsHighs.concat(pointsLows.reverse());
+    var areaFunction = d3.area()
+    .curve(d3.curveBasis)
+    .x(d => d[0])
+    .y1(d => d[1])
+    .y0(0);
+
+    this.svg
+      .append("path")
+      .attr("fill", "rgba(70, 130, 180, 0.2)")
+      .attr("d", areaFunction(pointsArea));  
 
     // Plot the dots
-    this.svg.append("g")
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => x(d.time))
-    .attr("cy", d => y(d.high))
-    .classed("chart-point", true);
-    }
+    // this.svg.append("g")
+    // .selectAll("dot")
+    // .data(data)
+    // .enter()
+    // .append("circle")
+    // .attr("cx", d => x(d.time))
+    // .attr("cy", d => y(d.average()))
+    // .classed("chart-point", true);
+
+  }
+
+  onSelectCoin(coinId: any) {
+    console.log("onSelectCoin(" + coinId + ") CALLED");
+
+    this.coins.forEach(coin => {
+      if(coinId == coin.id) {
+        coin.active = !coin.active;
+      }
+    });
+
+    let logMessage: string = "";
+    this.coins.forEach(coin => {
+      logMessage += coin.id + ": " + coin.active + ", ";
+    });
+    console.log(logMessage);
+  }
 
 }
