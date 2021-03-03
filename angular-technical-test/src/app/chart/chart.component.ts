f39a5cb81bf3d34d6e0d59bc822bc5f2f1c66018
Rob-Willson <ribrob@gmail.com>
2021-03-03 09:38:05 +0000

    Added basic scrubbing on the chart

--- angular-technical-test/src/app/chart/chart.component.ts
+++ angular-technical-test/src/app/chart/chart.component.ts
@@ -40,8 +40,7 @@ export class ChartComponent implements OnInit {
     45,
     90,
     180,
-    360,
-    720
+    360
   ];
 
   public selectedChartColor: ColorRGBA;
@@ -53,12 +52,13 @@ export class ChartComponent implements OnInit {
     new ColorRGBA(180, 0, 180, 1, "Magenta")
   ];
 
-  private svg;
-  private marginWidth: number = 60;
-  private marginHeight: number = 50;
-  private width: number = 800 - (this.marginWidth * 2);
-  private height: number = 500 - (this.marginHeight * 2);
   public linesSmooth: boolean = true;
+
+  private svg;
+  private marginWidth: number = 100;
+  private marginHeight: number = 50;
+  private width: number = 900 - (this.marginWidth * 2);
+  private height: number = 600 - (this.marginHeight * 2);
 
   constructor(private httpService : HttpService) {
     this.selectedCurrency = this.currencies[0];
@@ -68,7 +68,6 @@ export class ChartComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.createSvg();
     this.updatePlot();
   }
 
@@ -77,12 +76,12 @@ export class ChartComponent implements OnInit {
     d3.select("svg").remove();
 
     this.svg = d3
-    .select("figure#chart")
-    .append("svg")
-    .attr("width", this.width + (this.marginWidth * 2))
-    .attr("height", this.height + (this.marginHeight * 2))
-    .append("g")
-    .attr("transform", "translate(" + this.marginWidth + "," + this.marginHeight + ")");
+      .select("figure#chart")
+      .append("svg")
+      .attr("width", this.width + (this.marginWidth * 2))
+      .attr("height", this.height + (this.marginHeight * 2))
+      .append("g")
+      .attr("transform", "translate(" + this.marginWidth + "," + this.marginHeight + ")");
   }
 
   private updatePlot(): void {
@@ -90,36 +89,57 @@ export class ChartComponent implements OnInit {
       console.log("RAW DATA: ");
       console.log(data);
       let dataProcessedFromObservable = CryptoData.parseFromJSON(data);
+      this.createSvg();
       this.drawPlot(dataProcessedFromObservable);
     });
   }
   
   private drawPlot(data: CryptoData[]): void {
     // Create the X-axis band scale
+    // Multiply time number by 1000, because JS Date class uses milliseconds, instead of seconds
     const x = d3
-    .scaleTime()
-    .domain([d3.min(data, d => d.time) as number, d3.max(data, d => d.time) as number])
-    .range([0, this.width]);
+      .scaleTime()
+      .domain([d3.min(data, d => d.timeInMillis()) as number, d3.max(data, d => d.timeInMillis()) as number])
+      .range([0, this.width]);
 
     // Create the Y-axis band scale
     const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, d => d.high) as number])
-    .range([this.height, 0]);
+      .scaleLinear()
+      .domain([0, d3.max(data, d => d.high) as number])
+      .range([this.height, 0]);
 
+    //let xAxisTimeFormat = d3.timeFormat("%Y-%m-%d");
+    
     // Draw the X-axis to the DOM
     this.svg
-    .append("g")
-    .attr("transform", "translate(0," + this.height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+      .append("g")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(5,0)")
+      .style("text-anchor", "end");
+
+    // X-axis label
+    this.svg
+      .append("text")
+      .style("fill", "rgb(210, 210, 210)")
+      .attr("x", this.width / 2 )
+      .attr("y", this.height + 40)
+      .style("text-anchor", "middle")
+      .text("Date (from " + d3.min(data, d => d.dateFromTimestamp().toLocaleDateString() + " to " + d3.max(data, d => d.dateFromTimestamp().toLocaleDateString()) + ")"));
 
     // Draw the Y-axis to the DOM
     this.svg
-    .append("g")
-    .call(d3.axisLeft(y));
+      .append("g")
+      .call(d3.axisLeft(y));
+
+    // Y-axis label
+    this.svg.append("text")
+      .style("fill", "rgb(210, 210, 210)")
+      .attr("transform", "translate(-1)rotate(-90)")
+      .attr("x", (-this.height / 2) - this.marginHeight)
+      .attr("y", -50)
+      .text("Value (" + this.selectedCurrencyStandard + ")");
 
     this.plotData(x, y, data);
   }
@@ -127,17 +147,17 @@ export class ChartComponent implements OnInit {
   private plotData(x, y, data: CryptoData[]): void {
     // Draw the lines
     const points: [number, number][] = data.map(
-      d => [x(new Date(d.time)), y(d.average())]
+      d => [x(d.dateFromTimestamp()), y(d.average())]
     );
     this.drawLine(points, 2.5, 1.0);
     
     const pointsHighs: [number, number][] = data.map(
-      d => [x(new Date(d.time)), y(d.high)]
+      d => [x(d.dateFromTimestamp()), y(d.high)]
     );
     this.drawLine(pointsHighs, 1.5, 0.7);
 
     const pointsLows: [number, number][] = data.map(
-      d => [x(new Date(d.time)), y(d.low)]
+      d => [x(d.dateFromTimestamp()), y(d.low)]
     );
     this.drawLine(pointsLows, 1.5, 0.7);  
 
@@ -146,7 +166,7 @@ export class ChartComponent implements OnInit {
     this.drawArea(pointsArea, 0.2);
 
     // Draw the dots
-    //this.drawDots(x, y, data, 1);
+    this.drawScrubber(data);
   }
 
   private drawLine(points: [number, number][], width: Number, alpha: number): void {
@@ -168,11 +188,12 @@ export class ChartComponent implements OnInit {
   private drawArea(points: [number, number][], alpha: number) {
     let color: ColorRGBA = new ColorRGBA(this.selectedChartColor.r, this.selectedChartColor.g, this.selectedChartColor.b, alpha, this.selectedChartColor.descriptor);
     
-    var areaFunction = d3.area()
-    .curve(this.linesSmooth ? d3.curveBasis : d3.curveLinear)
-    .x(d => d[0])
-    .y1(d => d[1])
-    .y0(0);
+    let areaFunction = d3
+      .area()
+      .curve(this.linesSmooth ? d3.curveBasis : d3.curveLinear)
+      .x(d => d[0])
+      .y1(d => d[1])
+      .y0(0);
 
     this.svg
       .append("path")
@@ -180,45 +201,59 @@ export class ChartComponent implements OnInit {
       .attr("d", areaFunction(points));  
   }
 
-  private drawDots(x, y, data: CryptoData[], alpha: number) {
-    let color: ColorRGBA = new ColorRGBA(this.selectedChartColor.r, this.selectedChartColor.g, this.selectedChartColor.b, alpha, this.selectedChartColor.descriptor);
-    
-    this.svg.append("g")
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => x(d.time))
-    .attr("cy", d => y(d.average()))
-    .style("fill", color)
-    .classed("chart-point", true);
+  private drawScrubber(data: CryptoData[]) {
+    let splitWidth: number = this.width / this.selectedTimeframe;
+
+    for(let i = 0; i < data.length; i++) {
+      let svg = this.svg
+        .append("g")
+        .classed("chart-point", true); 
+      
+      svg
+        .append("rect")
+        .attr("x", splitWidth * i)
+        .attr("y", 0)
+        .attr("width", this.width / this.selectedTimeframe + 3)
+        .attr("height", this.height)
+        .style("fill", "white");
+      
+      svg
+        .append("text")
+        .text(data[i].dateFromTimestamp().toDateString())
+        .attr("x", (splitWidth) * i - (data[i].high.toString().length * 3))
+        .attr("y", -22)
+        .attr("fill", "white");
+
+      svg
+        .append("text")
+        .text(data[i].low + " - " + data[i].high)
+        .attr("x", (splitWidth) * i - (data[i].high.toString().length * 3))
+        .attr("y", -6)
+        .attr("fill", "white");
+    }
   }
 
   public onSelectCurrency(currency: any) {
     this.selectedCurrency = currency;
 
-    this.createSvg();
     this.updatePlot();
   }
 
   public onSelectCurrencyStandard(coinStandard: any) {
     this.selectedCurrencyStandard = coinStandard;
 
-    this.createSvg();
     this.updatePlot();
   }
 
   public onSelectTimeframe(timeframe: any) {
     this.selectedTimeframe = timeframe;
 
-    this.createSvg();
     this.updatePlot();
   }
 
   public onSelectColor(color: any) {
     this.selectedChartColor = color;
 
-    this.createSvg();
     this.updatePlot();
   }
 
@@ -226,8 +261,7 @@ export class ChartComponent implements OnInit {
     this.linesSmooth = !this.linesSmooth;
     console.log(this.linesSmooth);
 
-    this.createSvg();
     this.updatePlot();
   }
-
+  
 }
